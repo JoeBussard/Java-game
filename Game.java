6f768d29f812e270a2b91049d2c0a19002566c8a
OK Wasup <joeb360@gmail.com>
2016-07-25 15:23:39 -0700

    Hello world
    Added the game :100: :100: :100:

---
+++ Game.java
@@ -0,0 +1,55 @@
+/*****************************
+** Java game *****************
+*****************************/
+
+
+
+import java.io.*;
+import java.util.*;
+
+class Player {
+	int myNumber = 0;	//Guessed number
+	int theNumber;		//Real number
+	int tries			//Number of attempts
+	bool winner = false;
+	Random rand = new Random();
+	public void runGame(){
+		theNumber = rand.nextInt(10) +1;
+		setup();
+		while(!winner){
+		guess();
+		score();
+		}
+	}
+	void setup(){
+		System.println("Guess a number between 1 and 10\n");
+		System.println("Hint: The number is:" + theNumber);
+		}
+	void guess(){
+		myNumber = System.in.nextInt();
+		tries++;
+		}
+	void score(){
+		if (myNumber == theNumber){
+			System.println("Congrats you got it! It took you:");
+			if (tries==1){ System.println("1 try");}
+			else{System.println(tries + " tries");}
+		}
+		else {winner=true;}
+		}
+
+	}
+
+}
+
+class Game {
+	public static void main(String[] args)
+	{
+		Player player1 = new Player();
+
+		player1.runGame();
+
+		System.exit(0);
+
+	}
+}
